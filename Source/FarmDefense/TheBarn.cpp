000e52239709684b453660a6ea3b5062e2fa71cd
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-04-11 21:00:05 +0100

    mockup locked down

---
+++ Source/FarmDefense/TheBarn.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TheBarn.h"
+
+// Sets default values
+ATheBarn::ATheBarn()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATheBarn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATheBarn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
